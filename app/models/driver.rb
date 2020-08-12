369f139148f842e4d4bdda5b18a94ac215157dc9
Allen Shin <geeuho@gmail.com>
2020-08-11 19:03:06 -0700

    added validations to driver and shopper

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,12 @@
 class Driver < ApplicationRecord
     has_many :order
+    validates :username, presence: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :age, presence: true
+    validates :address, presence: true
+    validates :city, presence: true
+    validates :state, presence: true
+    validates :zip_code, presence: true
+    validates :email, presence: true
 end
