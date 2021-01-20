08a1d7e16d1090e9778820179a1e728c98eb521a
Allen <geeuho@gmail.com>
2021-01-19 19:54:12 -0800

    heroku test

--- app/controllers/stripe_payments_controller.rb
+++ app/controllers/stripe_payments_controller.rb
@@ -6,16 +6,28 @@ class StripePaymentsController < ApplicationController
         p ENV["STRIPE_TEST_PUBLISHABLE_KEY"]
         session = Stripe::Checkout::Session.create({
             payment_method_types: ['card'],
-            line_items: [{
-            price_data: {
-                currency: 'usd',
-                product_data: {
-                    name: 'T-shirt',
+            line_items: [
+                {
+                    price_data: {
+                        currency: 'usd',
+                        product_data: {
+                            name: 'T-shirt',
+                        },
+                        unit_amount: 2000,
+                    },
+                    quantity: 1,
                 },
-                unit_amount: 2000,
-            },
-            quantity: 1,
-            }],
+                {
+                    price_data: {
+                        currency: 'usd',
+                        product_data: {
+                            name: 'Bible',
+                        },
+                        unit_amount: 3000,
+                    },
+                    quantity: 1,
+                }
+            ],
             mode: 'payment',
             success_url: 'http://localhost:3001/orderpage',
             cancel_url: 'http://localhost:3001',
